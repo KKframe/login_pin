98912b731a6bb951934682927380cc1a5a5889a9
KKframe <keawsung.fame@gmail.com>
2022-01-28 00:22:42 +0700

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -28,25 +31,122 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
+        body: Container(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Column(
-            children: [
-              Container(
-                child: Icon(
-                  Icons.lock_outlined,
+          Expanded(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.lock_outlined,
+                      size: 75,
+                      color: Colors.deepPurple.shade500,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 10.0),
+                      child: Text(
+                        'กรุณาใส่รหัสผ่าน',
+                        style: TextStyle(
+                            fontSize: 30, color: Colors.deepPurple.shade500),
+                      ),
+                    ),
+                  ],
                 ),
-              )
-            ],
+              ],
+            ),
+          ),
+          
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    for (var i = 1; i <= 3; i++) _buildButton3(i)
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    for (var i = 4; i <= 6; i++) _buildButton3(i)
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    for (var i = 7; i <= 9; i++) _buildButton3(i)
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      height: 50,
+                      width: 40,
+                    ),
+                    _buildButton3(0),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 20.0),
+                      child: Icon(
+                        Icons.backspace_outlined,
+                      ),
+                    ),
+                  ],
+                ),
+                TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      'ลืมรหัสผ่าน',
+                      style: TextStyle(fontSize: 15),
+                      textAlign: TextAlign.center,
+                    ))
+              ],
+            ),
           )
         ],
-      )
-    );
+      ),
+    ));
   }
+}
+
+Widget _buildButton3(int n) {
+  return Padding(
+    padding: const EdgeInsets.all(8.0),
+    child: Container(
+      width: 60.0,
+      height: 60.0,
+      //color: Colors.green, // ห้ามกำหนด color ตรงนี้ ถ้าหากกำหนดใน BoxDecoration แล้ว
+      decoration: BoxDecoration(
+        color: Colors.deepPurple.shade200,
+        shape: BoxShape.circle,
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            n.toString(),
+            style: TextStyle(
+              fontSize: 20,
+            ),
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
+    ),
+  );
 }
